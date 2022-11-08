0134e5c2512256c31ea6a0da1c55ee306ac1de89
WHATSINYOURMIND <116942579+WHATSINYOURMIND@users.noreply.github.com>
2022-11-08 23:12:40 +0530

    Update squarelist.hs

--- haskell/squarelist.hs
+++ haskell/squarelist.hs
@@ -12,3 +12,28 @@ main = do
       let c=(read cin :: Int)
       let list1=[a,b,c]
       print(square list1)
+      
+      
+      
+      
+      second program
+      
+      --perform to square the element within a list
+square []=[]
+square(n:ns)=[n*n]++square(ns)
+main=do
+     putStrLn("Enter 1st element")
+     ain<-getLine
+     let a=(read ain::Int)
+     putStrLn("Enter 2nd element")
+     bin<-getLine
+     let b=(read bin::Int)
+     putStrLn("Enter 3rd element")
+     cin<-getLine
+     let c=(read cin::Int)
+     putStrLn("Enter 4th element")
+     din<-getLine
+     let d=(read din::Int)
+     let list=[a,b,c,d]
+     print(square list)
+     
